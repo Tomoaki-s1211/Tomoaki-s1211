dac5529459ee8d0d815cb5d93534dbcdb0ecf7f3
Tomoaki-s1211 <lineark.whiteglint365@gmail.com>
2020-08-09 18:11:25 +0900

    課題提出 PHP - パラメーターの受取(GETとPOST) 再提出

---
+++ parameter/parameterForm.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>parameterPostに送信するフォーム</title>
+</head>
+<body>
+    <form action="./parameterPost.php"
+    method="post">
+    str1:<input type="text" name="str1"/><br>
+    str2:<input type="text" name="str2"/><br>
+    str3:<input type="text" name="str3"/><br>
+    <input type="submit" value="表示">
+    </form>
+</body>
+</html>
